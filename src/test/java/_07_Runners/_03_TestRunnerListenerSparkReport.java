d91c834521cd73ed9cdf35ba4f05c9f128dce295
jacobyoldas <yasinyoldas6666@gmail.com>
2022-11-10 14:25:18 -0500

    Scroll Func Update

---
+++ src/test/java/_07_Runners/_03_TestRunnerListenerSparkReport.java
@@ -0,0 +1,20 @@
+package _07_Runners;
+
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
+
+
+@CucumberOptions(
+//    tags = "@Apache",
+//    tags = "@Regression",
+    tags = "@DBrun",
+    features = {"src/test/java/_01_FeatureFiles/"},
+    glue = {"_06_StepDefinitions"}
+)
+@Listeners({ExtentITestListenerClassAdapter.class}) //just listen and report logs
+
+public class _03_TestRunnerListenerSparkReport extends AbstractTestNGCucumberTests {
+
+}
