5b45e6d8ab665963cab99ba4f146ac0c5716d466
nigarasgarova <qiyasovanigar@gmail.com>
2022-11-16 14:36:31 +0400

    Academic Period/G-3_Tester_NQ

--- src/test/java/_06_StepDefinitions/_029_AcademicPeriod_Steps_NQ.java
+++ src/test/java/_06_StepDefinitions/_029_AcademicPeriod_Steps_NQ.java
@@ -46,7 +46,7 @@ public class _029_AcademicPeriod_Steps_NQ {
         nb.clickFunction(nb.getSchoolsAdminPage());
         dc.waitUntilVisibleAndClickable(dc.getNameInputAcPer());
         dc.clickFunction(dc.getNameInputAcPer());
-        dc.sendKeysFunction(dc.getNameInputAcPer(), "YASIN");
+        dc.sendKeysFunction(dc.getNameInputAcPer(), "YASIN YOLDAS");
         dc.clickFunction(dc.getSearchButton());
         Thread.sleep(2000);
         dc.waitUntilVisibleAndClickable(dc.getAcademicPeriodButton());
