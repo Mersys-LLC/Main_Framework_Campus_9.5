fcf44288cb9f78e6f059e37592967723197262e5
nigarasgarova <qiyasovanigar@gmail.com>
2022-11-30 18:12:28 +0400

    GradeLevels_API/G-1_Tester_NQ

---
+++ src/test/java/_04_API_POJO/Model/GradeLevels_NQ.java
@@ -0,0 +1,59 @@
+package _04_API_POJO.Model;
+
+public class GradeLevels_NQ {
+
+    private String name = "winter";
+    private String newName = "summer";
+    private String shortName = "win";
+    private String order = "1";
+    private String id;
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+
+
+
+}
